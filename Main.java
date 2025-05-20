7d69bc8cea39672e5f3b94c3e005743c782bbcf1
윤은빈 <yuneunbin133@gmail.com>
2025-05-20 20:20:26 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Main10 {
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
-        System.out.print("첫번쨰 숫자를 넣어주세요: ");
         int num1 = stdin.nextInt();
-
-        System.out.print("두번쨰 숫자를 넣어주세요(첫 번째 숫자보다 커야함): ");
         int num2 = stdin.nextInt();
 
         YEB_Calculator yc = new YEB_Calculator();
-        System.out.println("덧셈: " + yc.add(num1,num2));
-        System.out.println("곱셈: " + yc.multiply(num1,num2));
-        System.out.println("덧셈: " + Calculator.subtract(num2,num1));
+        System.out.println(yc.add(num1,num2));
+        System.out.println(yc.multiply(num1,num2));
+        System.out.println(Calculator.subtract(num2,num1));
     }
 }
 
